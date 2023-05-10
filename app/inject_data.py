5cac64834bcbf1bfb52688d3f36e18d84c825df2
paupaulchen <chaopong.chen@gmail.com>
2023-05-10 14:54:04 -0700

    efficient query to get film crew and genre

--- app/inject_data.py
+++ app/inject_data.py
@@ -6,15 +6,53 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 endpoint_url = "https://query.wikidata.org/sparql"
 
-query = """#Movies released in 2017
-SELECT DISTINCT ?item ?itemLabel ?imdbid ?pubdate WHERE {
-  ?item wdt:P31 wd:Q11424;
-        wdt:P345 ?imdbid.
-  ?item wdt:P577 ?pubdate.
-  FILTER(?pubdate >= "2019-01-01T00:00:00Z"^^xsd:dateTime)
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+query = """
+#Movies released in 2017
+SELECT DISTINCT
+  ?movie
+  ?movieLabel
+  ?imdbid
+  (MIN(?pubdate) as ?first_pubdate)
+  (MAX(?duration) as ?max_duration)
+  (GROUP_CONCAT(DISTINCT ?genre; SEPARATOR=", ") as ?genres)
+  (GROUP_CONCAT(DISTINCT ?director; SEPARATOR=", ") as ?directors)
+  (GROUP_CONCAT(DISTINCT ?screenwriter; SEPARATOR=", ") as ?screenwriters)
+  (GROUP_CONCAT(DISTINCT ?producer; SEPARATOR=", ") as ?producers)
+  (GROUP_CONCAT(DISTINCT ?cast_member; SEPARATOR=", ") as ?cast_members)
+WHERE {
+  {
+    SELECT DISTINCT ?movie ?movieLabel ?pubdate ?imdbid
+    WHERE {
+      ?movie wdt:P31 wd:Q11424.
+      ?movie wdt:P577 ?pubdate.
+      ?movie wdt:P345 ?imdbid.
+      FILTER (?pubdate >= "2017-01-01T00:00:00Z"^^xsd:dateTime &&
+              ?pubdate <= "2017-12-31T23:59:59Z"^^xsd:dateTime)
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+  }
+  OPTIONAL {
+    ?movie wdt:P2047 ?duration.
+  }
+  OPTIONAL {
+    ?movie wdt:P136 ?genre.
+  }
+  OPTIONAL {
+    ?movie wdt:P57 ?director.
+  }
+  OPTIONAL {
+    ?movie wdt:P58 ?screenwriter.
+  }
+  OPTIONAL {
+    ?movie wdt:P162 ?producer.
+  }
+  OPTIONAL {
+    ?movie wdt:P161 ?cast_member.
+  }
 }
-ORDER BY DESC(?pubdate)"""
+GROUP BY ?movie ?movieLabel ?imdbid
+ORDER BY ?pubdate
+"""
 
 
 def get_results(endpoint_url, query):
